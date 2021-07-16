d33cca31b749fcf53803d0d5727b1aaa7a6cbeb0
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-07-16 22:56:59 +0530

    Update footer section to use new style
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -1,53 +1,55 @@
 import React from 'react';
-import logo from '../../assets/img/logo.png';
+import PropTypes from 'prop-types';
 import Styles from './Footer.module.scss';
 import TextWithIcon from '../../components/TextWithIcon/TextWithIcon';
 import SocialIcon from '../../components/SocialIcon/SocialIcon';
-import {
-   faMapMarkerAlt,
-   faMobileAlt,
-   faEnvelope,
-} from '@fortawesome/free-solid-svg-icons';
-import {
-   faFacebookF,
-   faGithub,
-   faInstagram,
-   faLinkedinIn,
-   faYoutube,
-} from '@fortawesome/free-brands-svg-icons';
+import LinksWithHeader from '../../components/LinksWithHeader/LinksWithHeader';
 
 class Footer extends React.Component {
+   static propTypes = {
+      linkColumns: PropTypes.arrayOf({
+         header: PropTypes.string,
+         link: PropTypes.arrayOf({
+            text: PropTypes.string,
+            url: PropTypes.string,
+         }),
+      }),
+      textsWithIcon: PropTypes.arrayOf({
+         text: PropTypes.string,
+         icon: PropTypes.string,
+      }),
+      socialIcons: PropTypes.arrayOf({
+         link: PropTypes.string,
+         icon: PropTypes.string,
+      }),
+   };
    render() {
+      const linkColumns = this.props.linkColumns.map((link, index) => (
+         <LinksWithHeader header={link.header} links={link.links} key={index} />
+      ));
+      const textsWithIcon = this.props.textsWithIcon.map(
+         (textWithIcon, index) => (
+            <TextWithIcon
+               icon={textWithIcon.icon}
+               text={textWithIcon.text}
+               key={index}
+            />
+         ),
+      );
+      const socialIcons = this.props.socialIcons.map((socialIcon, index) => (
+         <SocialIcon
+            icon={socialIcon.icon}
+            link={socialIcon.link}
+            key={index}
+         />
+      ));
       return (
          <div className={Styles.container}>
-            <div className={Styles.leftPart}>
-               <div className={Styles.logoContainer}>
-                  <img className={Styles.logo} src={logo} />
-                  <span className={Styles.disclaimerText}>
-                     All ideas and source code will be under the ownership of
-                     DevSoc
-                  </span>
-               </div>
-               <span className={Styles.buttonPlaceholder} />
-               <span className={Styles.websiteCredits}>
-                  Website credits - ETHIndia and Devfolio
-               </span>
-               <span className={Styles.websiteEditor}>Edited by DevSoc</span>
-            </div>
-            <div className={Styles.rightPart}>
-               <span className={Styles.rightPartTitle}>Reach Us</span>
-               <TextWithIcon icon={faEnvelope} text="hello@xyz.co" />
-               <TextWithIcon icon={faMobileAlt} text="+91 82929 00001" />
-               <TextWithIcon
-                  icon={faMapMarkerAlt}
-                  text="BITS Pilani K K Birla Goa Campus"
-               />
+            <div className={Styles.links}>{linkColumns}</div>
+            <div className={Styles.contactUs}>
+               {textsWithIcon}
                <span className={Styles.socialIconsContainer}>
-                  <SocialIcon icon={faFacebookF} link="#" />
-                  <SocialIcon icon={faLinkedinIn} link="#" />
-                  <SocialIcon icon={faInstagram} link="#" />
-                  <SocialIcon icon={faYoutube} link="#" />
-                  <SocialIcon icon={faGithub} link="#" />
+                  {socialIcons}
                </span>
             </div>
          </div>
