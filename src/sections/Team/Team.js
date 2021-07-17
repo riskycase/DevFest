170dc79a0b4a871ab195ba7bc8cb26d308b97baa
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-07-17 10:31:13 +0530

    Update Team section to use new style
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src/sections/Team/Team.js
+++ src/sections/Team/Team.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import PersonCard from '../../components/personCard/personCard';
+import PersonCard from '../../components/PersonCard/PersonCard';
 import Styles from './Team.module.scss';
 
 class Team extends React.Component {
-   static propTypes = {
-      people: PropTypes.array,
-   };
    render() {
-      const people = this.props.people.map((person, index) => {
+      const team = [
+         {
+            name: 'Kanishq Khandelwal',
+            photo: '',
+            designation: 'Chief Coordinator',
+         },
+         {
+            name: 'Adithya',
+            photo: '',
+            designation: 'Sub-Coordinator',
+         },
+         {
+            name: 'Himanshu Jain',
+            photo: '',
+            designation: 'UI/UX Head',
+         },
+         {
+            name: 'Mayank Mathur',
+            photo: '',
+            designation: 'Web Dev Vertical Head',
+         },
+         {
+            name: 'Ritvij Kumar',
+            photo: '',
+            designation: 'Mobile Dev Vertical Head',
+         },
+         {
+            name: 'Yash Bhartia',
+            photo: '',
+            designation: 'Game Dev Vertical Head',
+         },
+      ].map((person, index) => {
          return (
             <div key={index} className={Styles.teamCard}>
                <PersonCard
                   name={person.name}
                   photo={person.photo}
-                  about={person.about}
+                  designation={person.designation}
                />
             </div>
          );
       });
-      return <div className={Styles.teamContainer}>{people}</div>;
+      return (
+         <div className={Styles.teamContainer}>
+            <span className={Styles.teamName}>The DevSoc Team</span>
+            <div className={Styles.teamMembers}>{team}</div>
+         </div>
+      );
    }
 }
 
