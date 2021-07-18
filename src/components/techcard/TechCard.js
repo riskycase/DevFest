e80495623635c9d5c9a3ba708a3f3c1df371703a
Anirudh Mukherjee <mukherjeeanirudh100@gmail.com>
2021-07-18 23:26:22 +0530

    Updated technology section styles

--- src/components/techcard/TechCard.js
+++ src/components/techcard/TechCard.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import Styles from './TechCard.module.scss';
 
 const TechCard = ({ title, img_url }) => {
+   const img = require('../../' + img_url);
    return (
       <div className={Styles.card}>
-         <img src={img_url} className={Styles.image} alt={title} />
-         <h3 className={Styles.title}>{title}</h3>
+         <img src={img.default} className={Styles.image} alt={title} />
+         {/* <h3 className={Styles.title}>{title}</h3> */}
       </div>
    );
 };
--- src/sections/technology/Technology.js
+++ src/sections/technology/Technology.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React /* , { useState } */ from 'react';
 import TechCard from '../../components/techcard/TechCard';
 import TechnologyList from './Tech.json';
 import Styles from './Technology.module.scss';
 
 const technology = () => {
+   /* const [itemsToShow, setItemsToShow] = useState(2);
+   const [buttonValue, setButtonValue] = useState('Show more');
+
+   const buttonClicked = () => {
+      if (itemsToShow == 2) {
+         setItemsToShow(TechnologyList.length);
+         setButtonValue('Show less');
+      } else {
+         setItemsToShow(2);
+         setButtonValue('Show more');
+      }
+   }; */
+
    return (
-      <div>
+      <div className={Styles.section}>
          <h2 className={Styles.heading}>Technologies in focus</h2>
          <div className={Styles.container}>
-            {TechnologyList.map(tech => (
+            {TechnologyList /* .slice(0, itemsToShow) */.map(tech => (
                <TechCard
                   key={tech.title}
                   title={tech.title}
@@ -16,6 +29,7 @@ const technology = () => {
                />
             ))}
          </div>
+         {/* <button onClick={() => buttonClicked()}>{buttonValue}</button> */}
       </div>
    );
 };
