d23b6a59d479c314abc918115894e027b01edb9a
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-07-19 23:50:08 +0530

    Revert "Update footer section to use new style"
    This reverts commit d33cca31b749fcf53803d0d5727b1aaa7a6cbeb0.

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -1,32 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import logo from '../../assets/img/logo.png';
 import Styles from './Footer.module.scss';
 import TextWithIcon from '../../components/TextWithIcon/TextWithIcon';
 import SocialIcon from '../../components/SocialIcon/SocialIcon';
-import LinksWithHeader from '../../components/LinksWithHeader/LinksWithHeader';
+import {
+   faMapMarkerAlt,
+   faMobileAlt,
+   faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+   faFacebookF,
+   faGithub,
+   faInstagram,
+   faLinkedinIn,
+   faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
 
 class Footer extends React.Component {
-   static propTypes = {
-      linkColumns: PropTypes.arrayOf({
-         header: PropTypes.string,
-         link: PropTypes.arrayOf({
-            text: PropTypes.string,
-            url: PropTypes.string,
-         }),
-      }),
-      textsWithIcon: PropTypes.arrayOf({
-         text: PropTypes.string,
-         icon: PropTypes.string,
-      }),
-      socialIcons: PropTypes.arrayOf({
-         link: PropTypes.string,
-         icon: PropTypes.string,
-      }),
-   };
    render() {
-      const linkColumns = this.props.linkColumns.map((link, index) => (
-         <LinksWithHeader header={link.header} links={link.link} key={index} />
-      ));
       const textsWithIcon = this.props.textsWithIcon.map(
          (textWithIcon, index) => (
             <TextWithIcon
@@ -45,8 +36,22 @@ class Footer extends React.Component {
       ));
       return (
          <div className={Styles.container}>
-            <div className={Styles.links}>{linkColumns}</div>
-            <div className={Styles.contactUs}>
+            <div className={Styles.leftPart}>
+               <div className={Styles.logoContainer}>
+                  <img className={Styles.logo} src={logo} />
+                  <span className={Styles.disclaimerText}>
+                     All ideas and source code will be under the ownership of
+                     DevSoc
+                  </span>
+               </div>
+               <span className={Styles.buttonPlaceholder} />
+               <span className={Styles.websiteCredits}>
+                  Website credits - ETHIndia and Devfolio
+               </span>
+               <span className={Styles.websiteEditor}>Edited by DevSoc</span>
+            </div>
+            <div className={Styles.rightPart}>
+               <span className={Styles.rightPartTitle}>Reach Us</span>
                {textsWithIcon}
                <span className={Styles.socialIconsContainer}>
                   {socialIcons}
