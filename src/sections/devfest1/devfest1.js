a66edebd537f19bddf78cc3ef2efcb37427d5f87
Darahaas Yajamanyam <darahaas15@gmail.com>
2021-07-25 13:29:50 +0530

    Space removed(on standby)

--- src/sections/devfest1/devfest1.js
+++ src/sections/devfest1/devfest1.js
@@ -15,39 +15,15 @@ function Devfest() {
                   <span className={DevfestStyle.sectionsubheadingdiff}>
                      Devfest 1.0
                   </span>{' '}
-                  was a massive success with 1000+ students and 5+ sessions.
-                  BITS Goa is back with yet another chapter of DEVFEST!
+                  was a massive success with people joining in!
+               </h4>
+               <h4 className={`text ${DevfestStyle.sectionsubheading2}`}>
+                  BITS Pilani, Goa is back with yet another chaper of DevFest!
                </h4>
             </div>
 
             <div className={`card ${DevfestStyle.card1}`}>
                <div className="row no-gutters">
-                  <div className={`col ${DevfestStyle.col1}`}>
-                     <div
-                        className={`card-block px-2 ${DevfestStyle.cardblock}`}
-                     >
-                        <h4
-                           className={`card-title ${DevfestStyle.cardtitle}`}
-                        ></h4>
-                        <p className={`card-text ${DevfestStyle.cardtext}`}></p>
-                     </div>
-                     <div
-                        className={`card-block px-2 ${DevfestStyle.cardblock}`}
-                     >
-                        <h4
-                           className={`card-title ${DevfestStyle.cardtitle}`}
-                        ></h4>
-                        <p className={`card-text ${DevfestStyle.cardtext}`}></p>
-                     </div>
-                     <div
-                        className={`card-block px-2 ${DevfestStyle.cardblock}`}
-                     >
-                        <h4
-                           className={`card-title ${DevfestStyle.cardtitle}`}
-                        ></h4>
-                        <p className={`card-text ${DevfestStyle.cardtext}`}></p>
-                     </div>
-                  </div>
                   <div className={`col-auto ${DevfestStyle.col2}`}>
                      <img src={Img1} className="img-fluid" alt="" />
                   </div>
