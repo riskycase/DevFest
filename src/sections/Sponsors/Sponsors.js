001592ee97b3872cffe755f08813e4e1b5b92da7
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2021-07-28 18:21:46 +0530

    Maintenance: updated sponsors and enabled register link

--- src/sections/Sponsors/Sponsors.js
+++ src/sections/Sponsors/Sponsors.js
@@ -4,7 +4,6 @@ import Logos from '../../components/Logos/Logos';
 import DevFolio from '../../assets/img/sponsors/Devfolio_Logo-White.svg';
 import Celo from '../../assets/img/sponsors/Celo Logo Color Reverse.svg';
 import Polygon from '../../assets/img/sponsors/Polygon_Logo-White.svg';
-import Portis from '../../assets/img/sponsors/Portis_Logo-Colored_White.svg';
 import Tezos from '../../assets/img/sponsors/Tezos_Logo-Colored.svg';
 import DigitalOcean from '../../assets/img/sponsors/DO_Logo_horizontal_blue.svg';
 import Replit from '../../assets/img/sponsors/Replit_dark-full.svg';
@@ -31,11 +30,6 @@ function Sponsors() {
                      name: 'Polygon',
                      image: Polygon,
                      link: 'https://polygon.technology/',
-                  },
-                  {
-                     name: 'Portis',
-                     image: Portis,
-                     link: 'https://www.portis.io/',
                   },
                   {
                      name: 'Tezos',
