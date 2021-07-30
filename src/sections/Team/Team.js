865d2e55256552b36c76022cf14b71f173ed7891
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2021-07-30 23:46:14 +0530

    Updated teams section

--- src/sections/Team/Team.js
+++ src/sections/Team/Team.js
@@ -6,6 +6,7 @@ import Himanshu from '../../assets/img/team/Himanshu.jpeg';
 import Adithya from '../../assets/img/team/Adithya.jpeg';
 import Yash from '../../assets/img/team/Yash_edited.jpeg';
 import Ritvij from '../../assets/img/team/Ritvij.png';
+import LinkedInIcon from './../../assets/img/linkedin.svg';
 
 class Team extends React.Component {
    render() {
@@ -14,31 +15,38 @@ class Team extends React.Component {
             name: 'Kanishq Khandelwal',
             photo: Kanishq,
             designation: 'Chief Coordinator',
+            linkedIn: 'https://www.linkedin.com/in/kanishqkhandelwal/',
          },
          {
-            name: 'Adithya',
+            name: 'Adithya M',
             photo: Adithya,
             designation: 'Sub-Coordinator',
+            linkedIn:
+               'https://www.linkedin.com/in/adithya-manickavasakam-5018b4193/',
          },
          {
             name: 'Himanshu Jain',
             photo: Himanshu,
-            designation: 'UI/UX Head',
+            designation: 'Product Design Head',
+            linkedIn: 'https://www.linkedin.com/in/himanshu-jain-18a85a1b1/',
          },
          {
             name: 'Mayank Mathur',
             photo: Mayank,
             designation: 'Web Dev Vertical Head',
+            linkedIn: 'https://www.linkedin.com/in/mayank-mathur-205767197/',
          },
          {
             name: 'Ritvij Kumar',
             photo: Ritvij,
             designation: 'Mobile Dev Vertical Head',
+            linkedIn: 'https://www.linkedin.com/in/rks-1410',
          },
          {
             name: 'Yash Bhartia',
             photo: Yash,
             designation: 'Game Dev Vertical Head',
+            linkedIn: 'https://www.linkedin.com/in/yashbhartia00',
          },
       ].map((person, index) => {
          return (
@@ -50,6 +58,11 @@ class Team extends React.Component {
                />
                <span className={Styles.name}>{person.name}</span>
                <span className={Styles.designation}>{person.designation}</span>
+               <span className={Styles.designation}>
+                  <a href={person.linkedIn}>
+                     <img src={LinkedInIcon} />
+                  </a>
+               </span>
             </div>
          );
       });
